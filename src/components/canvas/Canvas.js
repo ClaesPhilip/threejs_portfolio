98892a5d529d77051275d26d571fce5e3ef2e448
Philip Löfgren <55080120+ClaesPhilip@users.noreply.github.com>
2021-06-15 14:56:03 +0200

    added the basics of the planets

--- src/components/canvas/Canvas.js
+++ src/components/canvas/Canvas.js
@@ -1,6 +1,5 @@
-import { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls, Environment, Stars } from '@react-three/drei';
+import { OrbitControls, Stars, RoundedBox } from '@react-three/drei';
 
 import "../../styles/Canvas.css";
 
@@ -8,22 +7,17 @@ export default function CanvasComponent() {
   return (
     <div className="canvas-container">
       <Canvas>
-
         <OrbitControls />
         <ambientLight intensity={1} />
-        <directionalLight color="red" position={[0, 0, 5]} />
+        <directionalLight color="red" position={[0, 10, 5]} />
 
-        <Suspense fallback={null}>
-          {/* <Model /> */}
-          <Environment
-            background={false}
-            files={['px.png', 'nx.png', 'py.png', 'ny.png', 'pz.png', 'nz.png']}
-            path={'/'}
-            preset={null}
-            scene={undefined} // adds the ability to pass a custom THREE.Scene
-          />
-          <Stars radius={100} depth={50} count={5000} factor={4} saturation={0} fade />
-        </Suspense>
+        {/* STARS */}
+        <Stars radius={100} depth={50} count={5000} factor={4} saturation={0} fade />
+
+        <RoundedBox args={[1, 1, 1]} radius={0.05} smoothness={4}>
+          <meshPhongMaterial attach="material" color="#f3f3f3" wireframe />
+        </RoundedBox>
+
 
       </Canvas>
     </div>
