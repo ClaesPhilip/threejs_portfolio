6f14b51aba91636087e4495c3f5e9b7bf2045569
Philip Löfgren <55080120+ClaesPhilip@users.noreply.github.com>
2021-06-22 22:42:48 +0200

    testing stuff

--- src/components/canvas/CanvasStars.js
+++ src/components/canvas/CanvasStars.js
@@ -5,6 +5,8 @@ import { Canvas, useThree, useFrame } from "@react-three/fiber";
 import { useSpring, animated as a } from "@react-spring/three";
 import { Stars, RoundedBox, OrbitControls } from '@react-three/drei';
 import * as THREE from "three";
+// import JSONfont from "D:/KYH PLUGG/Folders/threejs_portfolio/src/Roboto_Regular.json";
+
 
 export default function CanvasThing() {
 
@@ -43,35 +45,47 @@ export default function CanvasThing() {
     })
     return <OrbitControls autoRotate target={[0, 0, 0]} args={[camera, gl.domElement]} />
   }
+
+//   loader.load( 'fonts/helvetiker_regular.typeface.json', function ( font ) {
+
+// 	const geometry = new THREE.TextGeometry( 'Hello three.js!', {
+// 		font: font,
+// 		size: 80,
+// 		height: 5,
+// 		curveSegments: 12,
+// 		bevelEnabled: true,
+// 		bevelThickness: 10,
+// 		bevelSize: 8,
+// 		bevelOffset: 0,
+// 		bevelSegments: 5
+// 	} );
+// } );
   
   
-  function TextMesh(props) {
-    const mesh = useRef();
-    const starwarsText = "It is a period of civil wars in the galaxy. A brave alliance of underground freedom fighters has challenged the tyranny and oppression of the awesome GALACTIC EMPIRE. Striking from a fortress hidden among the billion stars of the galaxy, rebel spaceships have won their first victory in a battle with the powerful Imperial Starfleet. The EMPIRE fears that another defeat could bring a thousand more solar systems into the rebellion, and Imperial control over the galaxy would be lost forever. To crush the rebellion once and for all, the EMPIRE is constructing a sinister new battle station. Powerful enough to destroy an entire planet, its completion spells certain doom for the champions of freedom.";
-  
-    // actions to perform in current frame
-    useFrame(() => {
-      mesh.current.rotation.x = mesh.current.rotation.y += 0.01;
-      mesh.current.geometry.center();
-    });
-  
-    // configure font mesh
-    const textOptions = {
-      size: 5,
-      height: 1
-    };
-  
-    
-  
-    return (
-      <mesh
-        {...props}
-        ref={mesh}
-      >
-        <textGeometry attach="geometry" args={[starwarsText, textOptions]} />
-      </mesh>
-    );
-  }
+  // function TextMesh (props) {
+  //   const mesh = useRef();
+  //   const starwarsText = "It is a period of civil wars in the galaxy. A brave alliance of underground freedom fighters has challenged the tyranny and oppression of the awesome GALACTIC EMPIRE. Striking from a fortress hidden among the billion stars of the galaxy, rebel spaceships have won their first victory in a battle with the powerful Imperial Starfleet. The EMPIRE fears that another defeat could bring a thousand more solar systems into the rebellion, and Imperial control over the galaxy would be lost forever. To crush the rebellion once and for all, the EMPIRE is constructing a sinister new battle station. Powerful enough to destroy an entire planet, its completion spells certain doom for the champions of freedom.";
+  //   const font = new THREE.FontLoader();
+
+  //   font.load("D:/KYH PLUGG/Folders/threejs_portfolio/src/Roboto_Regular.json", function (fonten) {
+  //     const geometry = new THREE.TextGeometry(starwarsText, {
+  //       font: fonten,
+  //       size: 80,
+  //       height: 5,
+  //       curveSegments: 12,
+  //       bevelEnabled: true,
+  //       bevelThickness: 10,
+  //       bevelSize: 8,
+  //       bevelOffset: 0,
+  //       bevelSegments: 5
+  //     });
+  //     return (
+  //       <mesh {...props} ref={mesh}>
+  //         <textGeometry attach="geometry" args={[starwarsText, geometry]} />
+  //       </mesh>
+  //     );
+  //   });
+  // }
 
 
   return (
@@ -90,10 +104,10 @@ export default function CanvasThing() {
           <p>{starwarsText}</p>
         </a.div> */}
 
-        <Suspense fallback={null}>
+        {/* <Suspense fallback={null}>
           <ambientLight intensity={1} />
           <TextMesh position={{...propsText}} />
-        </Suspense>
+        </Suspense> */}
 
         <Controls/>
       </Canvas>
